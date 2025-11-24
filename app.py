d41bd813991264bb0c07b863cae6a43b9b46d630
yiningy890-beep <yiningy890@gmail.com>
2025-11-24 07:59:41 +0000

    1

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-from flask import Flask,render_template,request
+from flask import Flask, request, render_template
+import joblib
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return(render_template("index.html"))
+    return render_template("index.html")
 
-@app.route("/main",methods=["GET","POST"])
+@app.route("/main", methods=["GET", "POST"])
 def main():
     q = request.form.get("q")
     print(q)
-    return(render_template("main.html"))
+    return render_template("main.html")
 
-if __name__=="__main__":
+@app.route("/dbs", methods=["GET", "POST"])
+def dbs():
+    return render_template("dbs.html")
+
+@app.route("/dbs_prediction", methods=["GET", "POST"])
+def dbs_prediction():
+    q = float(request.form.get("q"))
+    print(q)
+    model=joblib.load("dbs_sgd.pkl")
+    r = model.predict([[q]])
+    return render_template("dbs_prediction.html",r=r[0][0])
+
+
+@app.route("/creditability", methods=["GET", "POST"])
+def creditability():
+    return(render_template("creditability.html"))
+
+if __name__ == "__main__":
     app.run()
